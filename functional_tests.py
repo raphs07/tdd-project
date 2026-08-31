873e7f0a87961ad9cfa799d5ff7bcba272cdca9b
Raphaela Sousa <raphaaeladesousa07@gmail.com>
2026-08-30 22:48:40 -0300

    Melhorando a classe de teste com setUp e tearDown

--- functional_tests.py
+++ functional_tests.py
@@ -4,15 +4,17 @@ import unittest
 
 class NewVisitorTest(unittest.TestCase):
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
-
+    def setUp(self):
         self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
 
         self.browser.get('http://localhost:8000')
 
         self.assertIn('To-Do', self.browser.title)
-
-        self.browser.quit()
 
 
 if __name__ == '__main__':
