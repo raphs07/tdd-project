221002c5edbd3d549fea0a654fa82d1d54ae46a9
Raphaela Sousa <raphaaeladesousa07@gmail.com>
2026-09-07 20:08:33 -0300

    Utilizando Selenium para Testar Interações do Usuário

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
 import unittest
 
 
@@ -12,9 +15,53 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
 
+        # Maria decidiu utilizar o novo app TODO.
+        # Ela entra em sua página principal:
         self.browser.get('http://localhost:8000')
 
+        # Ela nota que o título da página menciona TODO
         self.assertIn('To-Do', self.browser.title)
+
+        header_text = self.browser.find_element(
+            By.TAG_NAME, 'h1'
+        ).text
+
+        self.assertIn('To-Do', header_text)
+
+        # Ela é convidada a entrar com um item TODO imediatamente
+        inputbox = self.browser.find_element(
+            By.ID, 'id_new_item'
+        )
+
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # Ela digita "Estudar testes funcionais"
+        inputbox.send_keys('Estudar testes funcionais')
+
+        # Ela aperta Enter
+        inputbox.send_keys(Keys.ENTER)
+
+        # Espera a página atualizar
+        time.sleep(1)
+
+        # A tarefa deve aparecer na tabela
+        table = self.browser.find_element(
+            By.ID, 'id_list_table'
+        )
+
+        rows = table.find_elements(
+            By.TAG_NAME, 'tr'
+        )
+
+        self.assertTrue(
+            any(
+                row.text == '1: Estudar testes funcionais'
+                for row in rows
+            )
+        )
 
 
 if __name__ == '__main__':
