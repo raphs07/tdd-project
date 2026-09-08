97292775eeb8fc7f818cc484c000eac830eee64a
Raphaela Sousa <raphaaeladesousa07@gmail.com>
2026-09-07 21:21:08 -0300

    Persistindo os Dados

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,8 @@
-from django.urls import resolve
 from django.test import TestCase
+from django.urls import resolve
 
 from lists.views import home_page
+from lists.models import Item
 
 
 class HomePageTest(TestCase):
@@ -12,7 +13,6 @@ class HomePageTest(TestCase):
 
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
-
         html = response.content.decode('utf8')
 
         self.assertTrue(html.startswith('<html>'))
@@ -23,13 +23,42 @@ class HomePageTest(TestCase):
 
     def test_can_save_a_POST_request(self):
         response = self.client.post(
-        '/',
-        data={'item_text': 'A new list item'}
-    )
+            '/',
+            data={'item_text': 'A new list item'}
+        )
 
         self.assertIn(
-        'A new list item',
-        response.content.decode()
-    )
+            'A new list item',
+            response.content.decode()
+        )
 
         self.assertTemplateUsed(response, 'home.html')
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'O primeiro item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'O segundo item'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+
+        self.assertEqual(
+            first_saved_item.text,
+            'O primeiro item'
+        )
+
+        self.assertEqual(
+            second_saved_item.text,
+            'O segundo item'
+        )
