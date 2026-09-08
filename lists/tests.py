e53031805e05b7ec2046d6eb1a5788ee8091b693
Raphaela Sousa <raphaaeladesousa07@gmail.com>
2026-09-07 21:33:13 -0300

    Refatorando o Teste de Unidade

--- lists/tests.py
+++ lists/tests.py
@@ -27,6 +27,15 @@ class HomePageTest(TestCase):
         data={'item_text': 'A new list item'}
     )
 
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_can_save_a_POST_request_with_item(self):
+        self.client.post(
+        '/',
+        data={'item_text': 'A new list item'}
+    )
+
         self.assertEqual(Item.objects.count(), 1)
 
         new_item = Item.objects.first()
@@ -35,14 +44,6 @@ class HomePageTest(TestCase):
         new_item.text,
         'A new list item'
     )
-
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
-
-    def test_only_saves_items_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
-
 
 class ItemModelTest(TestCase):
 
